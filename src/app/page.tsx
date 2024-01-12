ba6650b60343c2bb6feeaaed2d48f2884b258423
Souza0Rock <mateusmsouza04@gmail.com>
2024-01-11 23:56:42 -0300

    backup for ssr in component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,37 +8,14 @@ import { useEffect } from "react";
 import GridGenders from "@/components/sections/GridGenders";
 import { getBooksGenders } from "@/services/fetch/getBooksGenders";
 
-// const getData = async () => {
-//   const data = await getBooksGenders();
-
-//   return { data };
-// };
-
-async function getProjects() {
-  const res = await fetch(`https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=Sd3Scmz7lLmxJU8aObe2oie9KSfbnyBB`)
-  const projects = await res.json()
- 
-  return projects
-}
 
 export default async function Home() {
-  // const { push } = useRouter()
+  // const projects = await getBooksGenders()
 
-  // useEffect(() => {
-  //   getBooksGenders();
-  // }, [])
-
-  // console.log(teste, "teste");
-
-  // const data = await getBooksGenders()
-
-  // console.log(data, "data")
-  const projects = await getProjects()
-
-  console.log(projects, "projects")
+  // console.log(projects, "projects")
   return (
     <>
-    {projects.results.map((i) => <p style={{ color: "black" }}>{i.list_name}</p>)}
+    {/* {projects.results.map((i) => <p style={{ color: "black" }}>{i.list_name}</p>)} */}
       <GridGenders />
     </>
   );
